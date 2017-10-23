5ef99f07ff392f717b648c4da6878d39b5d63aae
jiangyong <jiangyong@didichuxing.com>
2017-10-23 11:18:26 +0800

    add test

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/smbrave/mysql-kafka/tunnel"
 )
 
+var (
+	flag_mysql_addr  = flag.String("mysql_addr", "127.0.0.1:3306", "mysql addr")
+	flag_mysql_user  = flag.String("mysql_user", "root", "msyql user")
+	flag_mysql_pass  = flag.String("mysql_pass", "root", "mysql password")
+	flag_kafka_topic = flag.String("kafka_topic", "test", "kafka topic")
+	flag_kafka_addr  = flag.String("kafka_addr", "127.0.0.1:9092", "kafka addr split by ',' ")
+)
+
 func main() {
+	flag.Parse()
+
 	cfg := tunnel.NewConfig()
-	cfg.MysqlAddr = "10.95.116.81:3306"
-	cfg.MysqlUser = "root"
-	cfg.MysqlPass = "123456"
-	cfg.KafkaTopic = "test"
-	cfg.KafkaAddr = "10.95.136.254:9092"
+	cfg.MysqlAddr = *flag_mysql_addr
+	cfg.MysqlUser = *flag_mysql_user
+	cfg.MysqlPass = *flag_mysql_pass
+	cfg.KafkaTopic = *flag_kafka_topic
+	cfg.KafkaAddr = *flag_kafka_addr
 
 	tunel := tunnel.NewTunnel(cfg)
 	err := tunel.Start()
---
+++ test/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/smbrave/mysql-kafka/tunnel"
+)
+
+func main() {
+	cfg := tunnel.NewConfig()
+	cfg.MysqlAddr = "10.95.116.81:3306"
+	cfg.MysqlUser = "root"
+	cfg.MysqlPass = "123456"
+	cfg.KafkaTopic = "test"
+	cfg.KafkaAddr = "10.95.136.254:9092"
+
+	tunel := tunnel.NewTunnel(cfg)
+	err := tunel.Start()
+	if err != nil {
+		panic(err)
+	}
+	select {}
+	tunel.Stop()
+}
